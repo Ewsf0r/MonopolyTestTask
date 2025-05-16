6303392cedac86174c1f46b1f57f1560cbe957cc
NikolayKorsakov <ewsf0r@gmail.com>
2025-05-16 21:36:33 +0300

    добавлена структура данных

---
+++ MonopolyTestTask/Model/Box.cs
@@ -0,0 +1,41 @@
+﻿namespace MonopolyTestTask.Model
+{
+    public record Box
+    {
+        private const int expirationDurationDays = 100;
+        public int Id { get; }
+        public double Width { get; }
+        public double Height { get; }
+        public double Depth { get; }
+        public double Weight { get; }
+        public double Volume { get; }
+        public DateOnly? ProductionDate { get; }
+        public DateOnly ExpirationDate { get; }
+        public Box(
+            int id,
+            double width,
+            double height,
+            double depth,
+            double weight,
+            DateOnly? productionDate = null,
+            DateOnly? expirationDate = null
+            )
+        {
+            if (productionDate == null && expirationDate == null)
+                throw new ArgumentNullException();
+            else if (productionDate != null) 
+            {
+                ProductionDate = productionDate;
+                ExpirationDate = productionDate.Value.AddDays(expirationDurationDays);
+            }
+            else
+                ExpirationDate = expirationDate!.Value;
+            Id = id;
+            Width = width;
+            Height = height;
+            Depth = depth;
+            Weight = weight;
+            Volume = Width * Height * Depth;
+        }
+    }
+}
---
+++ MonopolyTestTask/Model/Pallet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+
+namespace MonopolyTestTask.Model
+{
+    public class Pallet
+    {
+        private const double emptyWeight = 30;
+        public int Id { get; }
+        public double Width { get; }
+        public double Height { get; }
+        public double Depth { get; }
+        public double Weight { get; }
+        public double Volume { get; }
+        public DateOnly ExpirationDate { get; }
+        public ImmutableList<Box> Boxes { get; }
+        public Pallet(
+            int id,
+            double width,
+            double height,
+            double depth,
+            IEnumerable<Box> boxes
+            )
+        {
+            //Assuming empty pallet is an error
+            if (boxes?.Any(_box => _box != null) == false)
+                throw new ArgumentNullException("Pallet must contain some boxes");
+            Boxes = boxes!.ToImmutableList();
+            var largeBoxes = Boxes
+                .Where(_box => _box.Width > width || _box.Depth > depth)
+                .Select(_box => _box.Id.ToString());
+            if (largeBoxes.Any())
+                throw new ArgumentException($"Boxes {String.Join(", ", largeBoxes)} are too large for pallet {id}");
+            Id = id;
+            Width = width;
+            Height = height;
+            Depth = depth;
+            Weight = Boxes.Sum(_=>_.Weight) + emptyWeight;
+            Volume = Boxes.Sum(_ => _.Volume) + Width * Height * Depth;
+            ExpirationDate = Boxes.Max(_box => _box.ExpirationDate);
+        }
+    }
+}
